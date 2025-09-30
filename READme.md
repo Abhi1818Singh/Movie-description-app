cd453b695494d5aa857684e8e27bcfd4a5c4e81e
Abhi1818Singh <singhabhishekindradeo@gmail.com>
2025-09-30 23:47:07 +0530

    first commit

---
+++ READme.md
@@ -0,0 +1,26 @@
+A fun Minecraft-themed Movie Finder Web App where users can search for any movie and instantly fetch details like title, genre, IMDb rating, poster, and description.
+
+✨ Features:
+
+🔍 Search movies by name
+
+🖼️ Get posters, descriptions, genres, and IMDb ratings
+
+🎮 Minecraft-inspired pixelated UI with retro fonts
+
+🌐 Multi-language support (English, Hindi, Español)
+
+📌 Includes recommended movies section for quick picks
+
+⚡ Tech Stack:
+
+HTML, CSS, JavaScript
+
+API integration for real-time movie data
+
+
+
+
+
+About the Project
+This was built as a Hackathon project combining creativity (Minecraft design) with functionality (movie search). The aim was to create something both fun to use and informative.
---
+++ script.js
@@ -0,0 +1,62 @@
+const API_KEY = "18e29623";
+//  OMDb API key
+
+const searchBtn = document.querySelector("button");
+const searchInput = document.querySelector("input");
+const resultsContainer = document.querySelector(".results");
+
+searchBtn.addEventListener("click", searchMovies);
+
+async function searchMovies() {
+  event.preventDefault();
+  const query = searchInput.value.trim();
+  if (!query) return;
+
+  resultsContainer.innerHTML = "<h3>Loading...</h3>";
+
+  try {
+    const res = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
+    const data = await res.json();
+    
+
+
+    if (!data.Search) {
+      resultsContainer.innerHTML = "<h3>No results found</h3>";
+      return;
+    }
+
+    // Fetch full details for each movie
+    const movies = await Promise.all(
+      data.Search.map(async m => {
+        const detailsRes = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${m.imdbID}&plot=short`);
+        return await detailsRes.json();
+      })
+    );
+
+    // Display
+    resultsContainer.innerHTML = movies.map(m => `
+      <div class="movie-card">
+        <img src="${m.Poster !== "N/A" ? m.Poster : 'https://via.placeholder.com/300x450'}" alt="${m.Title}">
+        <h3>${m.Title} (${m.Year})</h3>
+        <p><strong>Genre:</strong> ${m.Genre}</p>
+        <p><strong>IMDb Rating:</strong> ${m.imdbRating}</p>
+        <p>${m.Plot}</p>
+      </div>
+    `).join("");// Join all movie cards into one big HTML string
+  } catch (err) {
+    resultsContainer.innerHTML = "<h3>Error fetching data</h3>";
+    console.error(err);
+  }
+}
+
+
+const selector = document.getElementById("language-selector");
+const elements = document.querySelectorAll("[data-key]");
+
+selector.addEventListener("change", (event) => {
+  const selectedLang = event.target.value;
+  elements.forEach(el => {
+    const key = el.getAttribute("data-key");
+    el.textContent = translations[selectedLang][key];
+  });
+});
